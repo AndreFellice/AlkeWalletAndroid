4df6e6241df25d6d2dbc5f5ec48c960de762b97c
andre <andreeecp@gmail.com>
2024-05-22 09:53:58 -0400

    Evaluacion 5 Instanciar pantallas Cambio a Kotlin view binding empezar dieseño arquitectura(MvvM)

--- app/src/main/java/com/example/alkewalletandroid/view/Pantalla3LoginActivity.kt
+++ app/src/main/java/com/example/alkewalletandroid/view/Pantalla3LoginActivity.kt
@@ -1,18 +1,29 @@
 package com.example.alkewalletandroid.view
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.example.alkewalletandroid.PassRecoveryFragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.example.alkewalletandroid.R
 import com.example.alkewalletandroid.databinding.ActivityPantalla3LoginBinding
+import com.example.alkewalletandroid.model.AppDataBase
+import com.example.alkewalletandroid.model.User
+import com.example.alkewalletandroid.viewmodel.Pantalla3LoginViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class Pantalla3Login : AppCompatActivity() {
+class Pantalla3LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPantalla3LoginBinding
+    private val viewModel: Pantalla3LoginViewModel by viewModels()
     private var isPasswordVisible = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +33,23 @@ class Pantalla3Login : AppCompatActivity() {
         binding = ActivityPantalla3LoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
+       // Configurar el manejo del botón de retroceso
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // Ocultar el teclado si está visible
+                hideKeyboard()
+                // Si deseas terminar la actividad después de ocultar el teclado
+                if (isEnabled) {
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
+        // Insertar usuario de ejemplo al iniciar la aplicación
+        insertarUsuarioFellice()
+
         binding.apply {
             ojo.setOnClickListener { togglePasswordVisibility() }
             btn3.setOnClickListener { loginUser() }
@@ -29,11 +57,17 @@ class Pantalla3Login : AppCompatActivity() {
             btn5.setOnClickListener { showPasswordRecoveryFragment() }
             logo3.setOnClickListener { navigateToMainActivity() }
         }
+        observeViewModel()
     }
 
+    private fun hideKeyboard() {
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        inputMethodManager?.hideSoftInputFromWindow(binding.root.windowToken, 0)
+    }
     private fun loginUser() {
         val email = binding.Email.text.toString()
         val password = binding.Password.text.toString()
+
         // Validación de campos de entrada
         if (email.isEmpty() || password.isEmpty()) {
             Toast.makeText(
@@ -43,17 +77,53 @@ class Pantalla3Login : AppCompatActivity() {
             ).show()
             return
         }
+        viewModel.login(email, password)
 
-        // Aquí deberías agregar la lógica para verificar el usuario y la contraseña
+    }
+    // Método para insertar un usuario de ejemplo en la base de datos
+    private fun insertarUsuarioFellice() {
+        lifecycleScope.launch (Dispatchers.IO){
+            val userDao = AppDataBase.getInstance(applicationContext).userDao()
+            val usuarioEjemplo = User(
+                id = 0, // Generar ID automáticamente
+                nombres = "Andre Fellice ",
+                apellidos = "Cuetos Picon",
+                email = "andreeecp@gmail.com",
+                password = "666999"
+            )
+            if (userDao.getUserByEmail("andreeecp@gmail.com") == null) {
+                userDao.insert(usuarioEjemplo)
+            }
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.navigateToMain.observe(this, Observer { navigate ->
+            if (navigate) {
+                val intent = Intent(this, Pantalla5HomeActivity::class.java)
+                startActivity(intent)
+                viewModel.doneNavigating()
+            }
+        })
+
+        viewModel.loginError.observe(this, Observer { isError ->
+            if (isError) {
+                Toast.makeText(
+                    this,
+                    "Correo electrónico o contraseña incorrectos",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
     }
     // metodo para acceder a pantalla de registro de nuevo cliente
     private fun navigateToSignUp() {
-        val intent = Intent(this, Pantalla4Signup::class.java)
+        val intent = Intent(this, Pantalla4SignupActivity::class.java)
         startActivity(intent)
     }
     // metodo para acceder a pantalla Welcome
  private fun navigateToMainActivity(){
-     val intent = Intent(this, Pantalla2Welcome::class.java)
+     val intent = Intent(this, Pantalla2WelcomeActivity::class.java)
      startActivity(intent)
  }
 
@@ -70,7 +140,7 @@ class Pantalla3Login : AppCompatActivity() {
         // Reemplazar el contenedor actual con el fragmento de recuperación de contraseña
         transaction.replace(R.id.fragment_container, recoveryFragment)
 
-        // Agregar la transacción a la pila de retroceso (opcional)
+        // Agregar la transacción a la pila de retroceso
         transaction.addToBackStack(null)
 
         // Confirmar la transacción
